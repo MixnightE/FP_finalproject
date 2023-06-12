9858ef246c7934fcccdf79f0797bd66e3ed8dec3
atidlod <ndhufrank@gmail.com>
2023-06-13 01:02:14 +0800

    new enemy mouse

--- include/CardAction.h
+++ include/CardAction.h
@@ -9,7 +9,12 @@
 void Incantation(Card *card, Player *player, Enemy *enemy, Field *field);
 // Cultist
 void DarkStrike(Card *card, Player *player, Enemy *enemy, Field *field);
-
+// Mouse
+void Chomp(Card *card, Player *player, Enemy *enemy, Field *field);
+// Mouse
+void Thrash(Card *card, Player *player, Enemy *enemy, Field *field);
+// Mouse
+void Bellow(Card *card, Player *player, Enemy *enemy, Field *field);
 /* cards for player */
 
 void Inflame(Card *card, Player *player, Enemy *enemy, Field *field);
